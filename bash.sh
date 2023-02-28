f8fca8a197600f1c086348dc9e716ecbd3ce1cb9
iglebekk <anders@iglebekk.no>
2023-02-28 14:20:15 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install composer php@8.1 mysql redis kubectl npm k6 wget
+brew doctor
+brew install --cask visual-studio-code tiles appcleaner postman slack tableplus microsoft-teams microsoft-word microsoft-powerpoint microsoft-excel transmit Flycut miro dbngin google-chrome
+composer global require laravel/installer laravel/valet spatie/phpunit-watcher
+valet install
+cd ~/
+wget 'https://raw.githubusercontent.com/iglebekk/.zshrc/master/.zshrc'
+source ~/.zshrc
