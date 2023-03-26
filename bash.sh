18c6aea64f2721bc188f141f212d3a448514df20
iglebekk <anders@iglebekk.no>
2023-03-26 13:54:41 +0200

    Update bash.sh
    added Laravel forge composer

--- bash.sh
+++ bash.sh
@@ -4,7 +4,7 @@
 brew install composer php@8.1 mysql redis kubectl npm k6 wget
 brew doctor
 brew install --cask visual-studio-code tiles appcleaner postman slack tableplus microsoft-teams microsoft-word microsoft-powerpoint microsoft-excel transmit Flycut miro dbngin google-chrome
-composer global require laravel/installer laravel/valet spatie/phpunit-watcher
+composer global require laravel/installer laravel/valet spatie/phpunit-watcher laravel/forge-cli
 valet install
 cd ~/
 wget 'https://raw.githubusercontent.com/iglebekk/.zshrc/master/.zshrc'
