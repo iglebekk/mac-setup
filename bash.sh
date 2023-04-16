3bfce2e52424213b728de04f1c5b12032c7ce5de
iglebekk <anders@iglebekk.no>
2023-04-16 20:49:50 +0200

    Update bash.sh
    added PostgreSQL

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install composer php@8.1 mysql redis kubectl npm k6 wget azure-cli
+brew install composer php@8.1 mysql redis kubectl npm k6 wget azure-cli postgresql
 brew doctor
 brew install --cask visual-studio-code tiles appcleaner postman slack tableplus microsoft-teams microsoft-word microsoft-powerpoint microsoft-excel transmit Flycut miro dbngin google-chrome
 composer global require laravel/installer laravel/valet spatie/phpunit-watcher laravel/forge-cli
