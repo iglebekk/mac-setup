da74b0035c02e192c82970e8a2c02f49bd37ecae
iglebekk <anders@iglebekk.no>
2024-05-29 13:30:26 +0200

    Update bash.sh
    postman out
    httpie in

--- bash.sh
+++ bash.sh
@@ -5,7 +5,7 @@ echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
 source ~/.zshrc
 brew install composer php@8.1 mysql redis npm wget postgresql
 brew doctor
-brew install --cask visual-studio-code tiles appcleaner postman slack tableplus microsoft-teams microsoft-word microsoft-powerpoint microsoft-excel transmit Flycut miro dbngin google-chrome
+brew install --cask visual-studio-code tiles appcleaner httpie slack tableplus microsoft-teams microsoft-word microsoft-powerpoint microsoft-excel transmit Flycut miro dbngin google-chrome
 composer global require laravel/installer laravel/valet spatie/phpunit-watcher laravel/forge-cli
 valet install
 cd ~/
