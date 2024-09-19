d32e3819417bd495586ebd228afa1e952da29217
iglebekk <anders@iglebekk.no>
2024-09-19 12:41:13 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,7 +5,7 @@ echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
 source ~/.zshrc
 brew install composer php@8.1 mysql redis npm wget postgresql
 brew doctor
-brew install --cask visual-studio-code tiles appcleaner httpie slack tableplus microsoft-teams microsoft-word microsoft-powerpoint microsoft-excel transmit Flycut miro dbngin google-chrome
+brew install --cask visual-studio-code appcleaner httpie slack tableplus microsoft-teams microsoft-word microsoft-powerpoint microsoft-excel transmit Flycut miro dbngin google-chrome
 composer global require laravel/installer laravel/valet spatie/phpunit-watcher laravel/forge-cli android-studio
 valet install
 cd ~/
