ec3877bef844030043db0f57b39ee5f55dcec667
iglebekk <anders@iglebekk.no>
2024-09-30 13:15:32 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -6,7 +6,7 @@ source ~/.zshrc
 brew install composer php@8.1 mysql redis npm wget postgresql
 brew doctor
 brew install --cask visual-studio-code appcleaner httpie slack tableplus microsoft-teams microsoft-word microsoft-powerpoint microsoft-excel transmit Flycut miro dbngin google-chrome
-composer global require laravel/installer laravel/valet spatie/phpunit-watcher laravel/forge-cli android-studio
+composer global require laravel/installer laravel/valet spatie/phpunit-watcher laravel/forge-cli android-studio dropbox
 valet install
 cd ~/
 rm -rf .zshrc
