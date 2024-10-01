b14f62cd4a836a6f343167d4f582b3d2e438560d
iglebekk <anders@iglebekk.no>
2024-10-01 11:23:21 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,7 +3,8 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
 source ~/.zshrc
-brew install composer php mysql redis npm wget postgresql watchman node watchman
+brew install composer php mysql redis npm wget postgresql watchman watchman
+brew install node@20 --force
 brew doctor
 brew install --cask visual-studio-code appcleaner httpie slack tableplus microsoft-teams microsoft-word microsoft-powerpoint microsoft-excel transmit Flycut miro dbngin google-chrome android-studio dropbox
 composer global require laravel/installer laravel/valet spatie/phpunit-watcher laravel/forge-cli 
