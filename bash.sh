9e8dd8c29d666ad196caa99409a9dedc0da1182b
iglebekk <anders@iglebekk.no>
2025-05-19 21:19:04 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -6,7 +6,7 @@ source ~/.zshrc
 brew install composer php mysql redis npm wget
 brew install node@20 --force
 brew doctor
-brew install --cask visual-studio-code appcleaner httpie slack tableplus microsoft-teams microsoft-word microsoft-powerpoint microsoft-excel transmit Flycut miro dbngin tiles google-chrome spotify monday.com
+brew install --cask visual-studio-code appcleaner httpie slack tableplus microsoft-teams microsoft-word microsoft-powerpoint microsoft-excel transmit Flycut miro dbngin tiles google-chrome spotify
 composer global require laravel/installer laravel/valet
 brew cleanup 
 valet install
